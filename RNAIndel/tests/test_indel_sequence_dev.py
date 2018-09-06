a6ba28b35324799b392cc63b05c6b99ef2789219
Kohei Hagiwara <kohei.hagiwara@stjude.org>
2018-09-06 13:03:59 -0500

    many cleanups done

---
+++ RNAIndel/tests/test_indel_sequence_dev.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3 
+
+from unittest import TestCase
+
+import sys
+sys.path.append('../rna_indel_lib')
+from  indelsequence_dev import Indel
+
+class TestIndel(TestCase):
+    
+    def setUp(self):
+        self.indel_1 = Indel('X', 200, 1, 'A')
+        self.indel_2 = Indel('chrX', 200, 1, 'A')
+
+    def test_chr(self):
+        self.assertEqual(self.indel_1.chr, 'chrX')
+        self.assertEqual(self.indel_2.chr, 'chrX')
+
+#class 
+
+
+if __name__ == '__main__':
+    from  unittest import main
+    main()
