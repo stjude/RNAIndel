5d14b54344d4f1701ccea3d6c3f2cd296b914e98
Kohei Hagiwara <kohei.hagiwara@stjude.org>
2019-01-16 20:47:59 -0600

    restructure step2.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("./rna_indel/version.py") as fp:
     exec(fp.read(), version)
 
 setup(
-    name="RNAIndel",
+    name="rna_indel",
     version=version["__version__"],
     description="Somatic indel discovery tool for tumor RNA-Seq data",
     url="https://github.com/stjude/RNAIndel",
@@ -20,15 +20,10 @@ setup(
     ],
     python_requires=">=3.5.2",
     packages=find_packages(exclude=["contrib", "docs", "tests"]),
-    package_data={
-        "rna_indel": ["bambino_lib/bambino-1.0.jar", "bambino_lib/mysql-connector-java-5.1.10.jar", "bambino_lib/picard.jar", "bambino_lib/third_party.jar"]
-    },
-    include_package_data=True,
-    test_suite="tests",
-    # I want have only one entry ponint
-    entry_points={
-        "console_scripts": [
-            "rna_indel=rna_indel.rna_indel:main",
-        ]
-    },
+    # package_data={
+    #    "rna_indel": ["bambino_lib/bambino-1.0.jar", "bambino_lib/mysql-connector-java-5.1.10.jar", "bambino_lib/picard.jar", "bambino_lib/third_party.jar"]
+    # },
+    # include_package_data=True,
+    # test_suite="tests",
+    entry_points={"console_scripts": ["rna_indel=rna_indel.rna_indel:main"]},
 )
